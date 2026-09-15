f023cb5e6ac1a2346a57123f34b6e203bc1c0288
Tu Nombre <tuemail@ejemplo.com>
2026-09-15 02:27:24 -0300

    FIIX FORM

--- veroweb/src/components/Contact.jsx
+++ veroweb/src/components/Contact.jsx
@@ -151,7 +151,6 @@ export default function Contact() {
             <div className="contact-socials">
               <a href="https://instagram.com/bajo_milente" target="_blank" rel="noreferrer">Instagram</a>
               <a href="https://wa.me/56900000000" target="_blank" rel="noreferrer">WhatsApp</a>
-              {/* <a href="https://behance.net" target="_blank" rel="noreferrer">Behance</a> */}
             </div>
           </div>
         </div>
